b1fc341740574bbee640b42d9eefae4d555d8344
Ron Stieger <ronstieger@gmail.com>
2014-10-19 18:35:35 -0700

    1.0 build 2, save recipes when going to background in case app terminates (and we aren't notified) refactor save to allow for saving other app state in future

--- Recipe Scaler/AppDelegate.swift
+++ Recipe Scaler/AppDelegate.swift
@@ -35,6 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
             self.recipes.append(Recipe())   // so we always have one to see in detail view
         }
         // TODO: remember the last recipe viewed instead of the first by default
+        // TODO?: go to it directly if iPhone?
         detailViewController.recipe = self.recipes[0]
 
         return true
@@ -43,11 +44,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        self.saveState()
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
@@ -60,7 +63,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        recipes.save(documentsUrl!)
+        self.saveState()
         // TODO: save itemToScale also? or most recent view?
     }
 
@@ -68,5 +71,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         return true
     }
     
+    func saveState() {
+        recipes.save(documentsUrl!)
+/*
+let splitViewController = self.window!.rootViewController as UISplitViewController
+        let detailNavigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as UINavigationController
+        if let detailViewController : ScalingViewController = detailNavigationController.viewControllers[0] as? ScalingViewController {
+            let recipeIndex = self.recipes.
+            NSKeyedArchiver.archiveRootObject(self, toFile: path!)
+        }
+*/
+    }
 }
 
